4784c8ae532f1dda720d9445cbf846b16682d1e4
Nicholas Tay <nexerq@gmail.com>
2017-10-06 13:47:29 +1100

    Global exception catching

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,13 @@ namespace MapleRIL
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+#if !DEBUG
+            e.Handled = true;
+            MessageBox.Show("A fatal error has occured in MapleRIL. Please screenshot and report this error to Nicholas Tay <nexerq@gmail.com>!\n\n" + e.Exception.ToString());
+            Environment.Exit(1);
+#endif
+        }
     }
 }
