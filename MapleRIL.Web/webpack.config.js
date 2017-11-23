5353385deae20220b451c0b8bdd8af6483341954
Nicholas Tay <nexerq@gmail.com>
2017-11-23 17:19:50 +1100

    Webpack auto copy js (for debugging)

--- MapleRIL.Web/webpack.config.js
+++ MapleRIL.Web/webpack.config.js
@@ -2,7 +2,11 @@
 
 var IS_PROD = process.env.NODE_ENV === "production";
 
+var fs = require("fs");
+var path = require("path");
 var webpack = require("webpack");
+
+var WebpackOnBuildPlugin = require("on-build-webpack");
 
 var config = {
     entry: "./js/main.js",
@@ -38,7 +42,24 @@ if (IS_PROD) {
             }
         }),
         new webpack.optimize.UglifyJsPlugin()
-    )
+    );
+} else {
+    // after the build copy to bin
+    var jsPath = path.join("Static", "js", "build.js");
+    config.plugins.push(
+        new WebpackOnBuildPlugin(function (stats) {
+            if (!fs.existsSync(path.resolve("./", "bin", "Debug")))
+                return;
+
+            fs
+                .createReadStream(path.resolve("./", jsPath))
+                .pipe(
+                    fs.createWriteStream(path.resolve("./", "bin", "Debug", jsPath))
+                );
+
+            console.log("(Also copied build to bin.)");
+        })
+    );
 }
 
 module.exports = config;
