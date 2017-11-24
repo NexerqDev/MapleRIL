6f722b0da2e0e64facd332e4327693c90eea752c
Nicholas Tay <nexerq@gmail.com>
2017-11-24 19:53:56 +1100

    ETC string.wz is weird af

---
+++ MapleRIL.Common/RILItemType/RILEtcItemItemType.cs
@@ -0,0 +1,21 @@
+﻿using MapleLib.WzLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapleRIL.Common.RILItemType
+{
+    public class RILEtcItemItemType : RILItemItemType
+    {
+        public RILEtcItemItemType() : base("Etc") { }
+
+        // for some reason etc is weird
+        public override List<WzImageProperty> GetAllStringIdProperties(RILFileManager files)
+        {
+            // String.wz/Etc.img/Etc/id
+            return files["String.wz"].WzDirectory.GetImageByName(Name + ".img")["Etc"].WzProperties;
+        }
+    }
+}
